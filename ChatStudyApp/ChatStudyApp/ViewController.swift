c01f547f2def2d8293871f0633f340960bc33f02
woong <workplayhard1@naver.com>
2023-08-01 17:50:33 +0900

    feat: 서버에 테스트 요청

--- ChatStudyApp/ChatStudyApp/ViewController.swift
+++ ChatStudyApp/ChatStudyApp/ViewController.swift
@@ -4,6 +4,8 @@
 //
 //  Created by 서현웅 on 2023/08/01.
 //
+// key정보
+// Subject: C=KR, ST=Seoul, L=Gumi, O=Wetudy, OU=StudyChat, CN=studychat.com/emailAddress=workplayhard1@naver.com
 
 import UIKit
 
@@ -11,9 +13,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        requestTest()
     }
 
-
+    func requestTest() {
+        let urlString = "https://studychat.com"
+        let url = URL(string: urlString)!
+        let urlRequest = URLRequest(url: url)
+        let urlSession = URLSession(configuration: .default)
+        
+        let task = urlSession.dataTask(with: urlRequest) { data, response, error in
+            print(data)
+            print(response)
+            print(error)
+        }
+        task.resume()
+    }
 }
 
