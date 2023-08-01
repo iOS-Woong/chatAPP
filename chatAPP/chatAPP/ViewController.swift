e62266b78071711ef334aa5979e3d31eeb242560
woong <workplayhard1@naver.com>
2023-08-01 17:02:33 +0900

    feat: 최종구현

--- chatAPP/chatAPP/ViewController.swift
+++ chatAPP/chatAPP/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        request()
     }
-
-
+    // https://127.0.0.1:8080/
+    func request() {
+        let url = URL(string: "https://chat.com")!
+        let urlRequest = URLRequest(url: url)
+        
+        let urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+        
+        let task = urlSession.dataTask(with: urlRequest) { data, response, error in
+            print("data:::", data)
+            print("response:::", response)
+            print("error:::", error)
+        }
+        task.resume()
+    }
 }
 
+extension ViewController: URLSessionDelegate {
+  func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+
+    //accept all certs when testing, perform default handling otherwise
+    completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
+  }
+}
