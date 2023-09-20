10123d39b073a2b0bccf2628a3a71dc907f0c77c
woong <workplayhard1@naver.com>
2023-09-20 18:41:54 +0900

    feat: DefaultNetworkRepository 생성

---
+++ ChatStudyApp/ChatStudyApp/Data/Network/Repository/DefaultNetworkRepository.swift
@@ -0,0 +1,12 @@
+//
+//  DefaultNetworkRepository.swift
+//  ChatStudyApp
+//
+//  Created by 서현웅 on 2023/09/20.
+//
+
+import Foundation
+
+final class DefaultNetworkRepository {
+    private let networkService = NetworkService()
+}
