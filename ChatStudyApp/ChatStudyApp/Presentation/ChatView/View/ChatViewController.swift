ed0495ac407bae6089c6370eb38dc1eb06a4ec9a
woong <workplayhard1@naver.com>
2023-12-06 21:05:31 +0900

    feat: chatView 코드 테스트코드 제거

--- ChatStudyApp/ChatStudyApp/Presentation/ChatView/View/ChatViewController.swift
+++ ChatStudyApp/ChatStudyApp/Presentation/ChatView/View/ChatViewController.swift
@@ -6,121 +6,12 @@
 //
 
 import UIKit
+import MessageKit
 
-class ChatViewController: UIViewController {
-    private let sendButton: UIButton = UIButton(frame: .zero)
-    let webSocket = WebSocket.shared
+class ChatViewController: MessagesViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        socket()
-        setupViews()
-        configureButton()
-        sendButtonAction()
-        view.backgroundColor = .white
-        requestPOST()
     }
-    
-    func request() {
-        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-        
-        let url = URL(string: "https://127.0.0.1:8080")!
-        let request = URLRequest(url: url)
-        
-        session.dataTask(with: request) { data, response, error in
-            print(data)
-        }.resume()
-    }
-    
-    func requestPOST() {
-        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-        
-        let url = URL(string: "https://127.0.0.1:8080/user_register")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        
-        let userToBeAdded = [
-            "name": "woong",
-            "imageUrl": "https://example.com/image.jpg",
-            "userDescription": "설명입니다"
-        ]
-        
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: userToBeAdded, options: [])
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        } catch {
-            print(error.localizedDescription)
-            return
-        }
-        
-        session.dataTask(with: request) { data, response, error in
-            print(String(data: data!, encoding: .utf8))
-        }.resume()
-    }
-    
-    func socket() {
-        try? webSocket.openWebSocket()
-        webSocket.delegate = self
-        
-        webSocket.onReceiveClosure = { (string, data) in
-            print(string, data)
-        }
-    }
-    
-    private func configureButton() {
-        sendButton.backgroundColor = .systemBlue
-        sendButton.setTitle("Send", for: .normal)
-        sendButton.setTitleColor(.black, for: .normal)
-    }
-    
-    private func setupViews() {
-        view.addSubview(sendButton)
-        
-        sendButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            sendButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-            sendButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
-            sendButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            sendButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    }
-    
-    private func sendButtonAction() {
-        let action = UIAction { [weak self] _ in
-            self?.webSocket.send(message: "hello world")
-        }
-        
-        sendButton.addAction(action, for: .touchUpInside)
-    }
-    
-}
-
-extension ChatViewController: URLSessionWebSocketDelegate {
-//    func urlSession(
-//        _ session: URLSession,
-//        webSocketTask: URLSessionWebSocketTask,
-//        didOpenWithProtocol protocol: String?)
-//    {
-//        print("OPEN")
-//    }
-//
-//    func urlSession(
-//        _ session: URLSession,
-//        webSocketTask: URLSessionWebSocketTask,
-//        didCloseWith closeCode: URLSessionWebSocketTask.CloseCode,
-//        reason: Data?)
-//    {
-//        print("CLOSE")
-//    }
-    
-    func urlSession(
-        _ session: URLSession,
-        didReceive challenge: URLAuthenticationChallenge,
-        completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?)
-        -> Void)
-    {
-        completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
-    }
-    
     
 }
